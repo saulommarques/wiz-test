0849c9b6e371257fe785ed04c5db710b4aaf44b8
Saulo Marques <saulo.marques@metropoles.com>
2020-10-20 11:57:55 -0300

    formulario e css reset

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'footer-app',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss', './../app.component.scss']
 })
 export class FooterComponent {
   title = 'wiz-test';
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'form-app',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss', './../app.component.scss']
 })
 export class FormComponent {
   title = 'wiz-test';
+
+  form = this.fb.group({
+    firstName: [''],
+    lastName: [''],
+  });
+
+  constructor(private fb: FormBuilder) { }
 }
